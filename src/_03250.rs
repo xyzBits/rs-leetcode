4cceca121725c5fb59dec2c2c760f91a11506101
xyzBits <lidf077@gmail.com>
2024-11-28 12:20:29 +0800

    3250 done

---
+++ src/_03250.rs
@@ -0,0 +1,31 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn count_of_pairs(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut dp = vec![vec![0; 51]; n];
+        let mod_num = 1000000007;
+
+        for v in 0..=nums[0] as usize {
+            dp[0][v] = 1;
+        }
+
+        for i in 1..n {
+            for v2 in 0..=nums[i] as usize {
+                for v1 in 0..=v2 {
+                    if nums[i - 1] - v1 as i32 >= nums[i] - v2 as i32 && nums[i] - v2 as i32 >= 0 {
+                        dp[i][v2] = (dp[i][v2] + dp[i - 1][v1]) % mod_num;
+                    }
+                }
+            }
+        }
+
+        let mut ans = 0;
+
+        for &v in &dp[n - 1] {
+            ans = (ans + v) % mod_num;
+        }
+
+        ans
+    }
+}
