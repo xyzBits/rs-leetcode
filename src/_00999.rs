ff9ad554483d5b529a4ffffdd7c0de98b7713f21
xyzBits <lidf077@gmail.com>
2024-12-06 17:34:19 +0800

    999 done

---
+++ src/_00999.rs
@@ -0,0 +1,38 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn num_rook_captures(board: Vec<Vec<char>>) -> i32 {
+        let (mut cnt, mut start, mut end) = (0, 0, 0);
+
+        let dx = vec![0, 1, 0, -1];
+        let dy = vec![1, 0, -1, 0];
+        for i in 0..8 {
+            for j in 0..8 {
+                if board[i][j] == 'R' {
+                    start = i;
+                    end = j;
+                    break;
+                }
+            }
+        }
+
+        for i in 0..4 {
+            let mut step = 0;
+            loop {
+                let tx = start + step * dx[i] as usize;
+                let ty = end + step * dy[i] as usize;
+                if tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B' {
+                    break;
+                }
+
+                if board[tx][ty] == 'p' {
+                    cnt += 1;
+                    break;
+                }
+                step += 1;
+            }
+        }
+
+        cnt
+    }
+}
