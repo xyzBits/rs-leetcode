d243f9f5e3b7dd9c3e7e0f3adff7a953881d89d9
xyzBits <lidf077@gmail.com>
2025-03-14 09:15:12 +0800

    3340 done

---
+++ src/_03340.rs
@@ -0,0 +1,15 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn is_balanced(num: String) -> bool {
+        let (mut diff, mut sign) = (0, 1);
+
+        for c in num.chars() {
+            let d = c.to_digit(10).unwrap() as i32;
+            diff += d * sign;
+            sign = -sign;
+        }
+
+        diff == 0
+    }
+}
