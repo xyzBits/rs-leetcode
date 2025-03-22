f66bcc7fdf852b16ebc00a1cec1978f41f65f6db
xyzBits <lidf077@gmail.com>
2025-03-22 08:16:29 +0800

    2643 done

---
+++ src/_02643.rs
@@ -0,0 +1,23 @@
+use crate::Solution;
+use std::cmp::max;
+
+impl Solution {
+    pub fn row_and_maximum_ones(mat: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut max_ones = 0;
+        let mut row_index = 0;
+
+        for i in 0..mat.len() {
+            let mut tot = 0;
+            for j in 0..mat[i].len() {
+                tot += mat[i][j];
+            }
+
+            if tot > max_ones {
+                max_ones = tot;
+                row_index = i;
+            }
+        }
+
+        vec![row_index as i32, max_ones]
+    }
+}
