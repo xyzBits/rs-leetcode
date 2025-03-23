02dcc974ba00062fb6c8a94fc04d7fa77c35aa22
xyzBits <lidf077@gmail.com>
2025-03-23 09:00:17 +0800

    2116 done

---
+++ src/_02116.rs
@@ -0,0 +1,33 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn can_be_valid(s: String, locked: String) -> bool {
+        let n = s.len();
+        let mut mx = 0;
+        let mut mn = 0;
+
+        let s_bytes = s.as_bytes();
+        let locked_bytes = locked.as_bytes();
+        for i in 0..n {
+            if locked_bytes[i] == b'(' {
+                let mut diff;
+                if s_bytes[i] == b'(' {
+                    diff = 1;
+                } else {
+                    diff = -1;
+                }
+                mx += diff;
+                mn = (mn + diff).max((i as i32 + 1) % 2);
+            } else {
+                mx += 1;
+                mn = (mn - 1).max((i as i32 + 1) % 2);
+            }
+
+            if mx < mn {
+                return false;
+            }
+        }
+
+        mn == 0
+    }
+}
