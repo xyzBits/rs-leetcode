a986b4219d29b65f0a07db7cccc2e8d87727e9c2
xyzBits <lidf077@gmail.com>
2025-05-27 08:53:58 +0800

    2894 done

---
+++ src/_02894.rs
@@ -0,0 +1,16 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let mut ans = 0;
+        for i in 1..=n {
+            if i % m == 0 {
+                ans -= i;
+            } else {
+                ans += i;
+            }
+        }
+
+        ans
+    }
+}
