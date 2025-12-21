3660a3e6b8afe1c65669cafb4ddb907c3f229200
xyzBits <lidf077@gmail.com>
2025-12-21 08:02:32 +0800

    728 done

--- src/_00728.rs
+++ src/_00728.rs
@@ -2,6 +2,27 @@ use crate::Solution;
 
 impl Solution {
     pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
-        todo!()
+        let mut ans = vec![];
+
+        for i in left..=right {
+            if Self::is_self_dividing(i) {
+                ans.push(i);
+            }
+        }
+
+        ans
+    }
+
+    fn is_self_dividing(num: i32) -> bool {
+        let mut temp = num;
+        while temp > 0 {
+            let digit = temp % 10;
+            if digit == 0 || num % digit != 0 {
+                return false;
+            }
+            temp /= 10;
+        }
+
+        true
     }
 }
---
+++ src/_00733.rs
@@ -0,0 +1,7 @@
+use crate::Solution;
+
+impl Solution {
+    pub fn flood_fill(image: Vec<Vec<i32>>, sr: i32, sc: i32, color: i32) -> Vec<Vec<i32>> {
+        todo!()
+    }
+}
